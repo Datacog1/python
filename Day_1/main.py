67ce4c84076a18702f86c346d7638bd5c2ac033c
Amna Rahman <amnarahman00@gmail.com>
2022-09-07 22:37:16 +0500

    Day 1 of 100 days of code Debugging Variable Initialization Input function Print function String

---
+++ Day_1/main.py
@@ -0,0 +1,11 @@
+from xml.etree.ElementTree import Comment
+
+
+#This is simple print command
+print("Hello world")
+
+#This adds newline character 
+print("Hello world\nHello world\nHello world")
+
+# String Concatenation
+print("Hello" + " "+ "Amna")
